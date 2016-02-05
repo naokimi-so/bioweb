c199381aec54f2b9e905063e53b184d8dd06dfa2
nandate <o238049t@mail.kyutech.jp>
2016-02-05 15:22:11 +0900

    とりあえず

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160127062922) do
+ActiveRecord::Schema.define(version: 20160127063640) do
 
   create_table "bios", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -21,8 +21,9 @@ ActiveRecord::Schema.define(version: 20160127062922) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
